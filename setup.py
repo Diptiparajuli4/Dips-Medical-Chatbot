06ce8249501ed55baaf506075638c3ed1864f09b
Dipti Parajuli <deeptiparajuli4@gmail.com>
2025-12-05 15:30:58 +0545

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Dips Mecical Chatbot",
+    version="0.1.0",    
+    author="Dipti Parajuli",
+    author_email="deeptiparajuli4@gmail.com ",
+    packages=find_packages(),
+    install_requires=[]
+)
