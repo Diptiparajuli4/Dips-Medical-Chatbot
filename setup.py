0e6821a3e640b83518a01c88602c59c50eca3f9b
Dipti Parajuli <deeptiparajuli4@gmail.com>
2025-12-11 19:16:32 +0545

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Dipti",
+    author_email="deeptiparajuli4@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
